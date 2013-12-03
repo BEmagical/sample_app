cde0462ddd8fe2a39da61d2e42d247effa8e812b
Brian Enriquez <benriquez@deloitte.com>
2013-12-03 13:55:36 -0800

    initial rails commit

---
+++ Gemfile
@@ -0,0 +1,35 @@
+source 'https://rubygems.org'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.1'
+
+group :development, :test do
+	gem 'sqlite3', '1.3.5'
+	gem 'rspec-rails', '2.9.0'
+end
+
+group :assets do
+	gem 'sass-rails', '~> 4.0.0'
+	gem 'uglifier', '>= 1.3.0'
+	gem 'coffee-rails', '~> 4.0.0'
+end
+
+gem 'jquery-rails'
+
+group :test do
+	gem 'capybara', '1.1.2'
+	gem 'rspec-rails', '2.9.0'
+end
+
+group :production do
+	gem 'pg', '0.12.2'
+end
+
+# Use unicorn as the app server
+# gem 'unicorn'
+
+# Use Capistrano for deployment
+# gem 'capistrano', group: :development
+
+# Use debugger
+# gem 'debugger', group: [:development, :test]
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '7c0b7a54441df14e417f2e374a1c8a963a142019c26a4d1b4150950125f327d584ba665d067b8add97060d6d7b965856c9fa5b0f42d5c4e750003f9134b01b7a'
