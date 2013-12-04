10c6a9ff27899579b260a7375d449132f24b469a
Brian Enriquez <benriquez@deloitte.com>
2013-12-04 15:56:12 -0800

    finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
   root  'static_pages#home'
 
   match '/signup',    to: 'users#new',              via: 'get'
@@ -8,7 +8,6 @@ SampleApp::Application.routes.draw do
   match '/contact',   to: 'static_pages#contact',   via: 'get'
 
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
