8603e5564c896bf07b53426ee1727fd34d7c411d
Brian Enriquez <benriquez@deloitte.com>
2013-12-11 12:05:37 -0800

    Add user following

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,3 +1,5 @@
+include ApplicationHelper
+
 def full_title(page_title)
   base_title = "[DD] Sample App"
   if page_title.empty?
@@ -9,6 +11,7 @@ end
 
 def sign_in(user, options={})
   if options[:no_capybara]
+    # Sign in when not using Capybara as well.
     remember_token = User.new_remember_token
     cookies[:remember_token] = remember_token
     user.update_attribute(:remember_token, User.encrypt(remember_token))
@@ -17,5 +20,11 @@ def sign_in(user, options={})
     fill_in "Email",    with: user.email
     fill_in "Password", with: user.password
     click_button "Sign in"
+  end
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
   end
 end
